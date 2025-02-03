2479c962760d83d975c6a074a40d4b72fed66946
Sunder Kumar <sunder-kirei@outlook.com>
2025-02-03 20:40:17 +0530

    add prisma

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,12 @@
-import { type NextAuthConfig } from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { type NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { prisma } from "./prisma";
 
 export default {
   providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
   pages: {
-    signIn: '/get-started',
+    signIn: "/get-started",
   },
 } satisfies NextAuthConfig;
