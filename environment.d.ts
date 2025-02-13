04d398b5b6653e32e256c1ae12b4ed256c04f0a5
Sunder Kumar <sunder-kirei@outlook.com>
2025-02-13 13:29:53 +0530

    add env typing

--- environment.d.ts
+++ environment.d.ts
@@ -1,5 +1,14 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_API_URL: string;
+    DATABASE_URL: string;
+
+    AUTH_GITHUB_ID: string;
+    AUTH_GITHUB_SECRET: string;
+
+    AUTH_GOOGLE_ID: string;
+    AUTH_GOOGLE_SECRET: string;
+
+    CLOUDINARY_URL: string;
   }
 }
